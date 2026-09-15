95ab342421a23674e7511dca5c5ad196e4025992
mahnoor <mahnoorwork74@gmail.com>
2026-09-15 22:51:17 +0500

    created files for app setup including core/app_colours.dart and core/app_typography.dart

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Backgrounds & Surface
+  static const Color darkBackground = Color(0xFF0D0E15);
+  static const Color darkCardBg = Color(0xFF181A24);
+  static const Color cardBorder = Color(0xFF262938);
+  static const Color inputBackground = Color(0xFF13151F);
+
+  // Accents & Gen-Z Branding
+  static const Color neonGreen = Color(0xFF00FF9D);
+  static const Color neonPurple = Color(0xFF9D00FF);
+  static const Color warningOrange = Color(0xFFFF9F43);
+  static const Color alertRed = Color(0xFFFF5252);
+
+  // Text Colors
+  static const Color textPrimary = Color(0xFFFFFFFF);
+  static const Color textSecondary = Color(0xFF98A0B3);
+  static const Color textMuted = Color(0xFF5E6578);
+}
